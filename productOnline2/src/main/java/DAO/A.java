412bc7e88aaf8f9b56cb0d3ed7c3e0908738082b
Ajay Jukanti <149111321+AJayJukanti@users.noreply.github.com>
2024-06-07 17:32:56 +0530

    Update A.java
    just for checking

--- productOnline2/src/main/java/DAO/A.java
+++ productOnline2/src/main/java/DAO/A.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 public class A {
 	public static void main(String[] args) {
 		try {
+			// Hello ......
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:ORCL","system","password");
 			System.out.println("A.main()");
